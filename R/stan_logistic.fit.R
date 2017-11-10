9c959acb48f9118f13811d7fe90a3cb38817dc00
gdepalma <gdepalma@apple.com>
2017-11-09 23:44:55 -0800

    Testing

--- R/stan_logistic.fit.R
+++ R/stan_logistic.fit.R
@@ -25,11 +25,12 @@ stan_logistic.fit <- function(xobs,yobs,xcens,ycens,xgrid) {
   xtrue=parms$xtrue
   
   ### Run Stan Model
-  # dat=list(y=yobs,xgrid=xgrid,Ngrid=Ngrid,n_groups=5,ysig=ysig,N=N,xobs=xobs,xsig=xsig,
-  #          xcensl=xcensl,xcensu=xcensu,ycensl=ycensl,ycensu=ycensu)
-  # init_fun <- function() {list(mu=seq(min(xtrue)+1,max(xtrue)-1,length=5),sigma=rep(1,5),
-  #           Theta=rep(1/5,5),xtrue=xtrue,coef=coefs)}
-  # stanfit <- stanmodels$logistic
+  dat=list(y=yobs,xgrid=xgrid,Ngrid=Ngrid,n_groups=5,ysig=ysig,N=N,xobs=xobs,xsig=xsig,
+           xcensl=xcensl,xcensu=xcensu,ycensl=ycensl,ycensu=ycensu)
+  init_fun <- function() {list(mu=seq(min(xtrue)+1,max(xtrue)-1,length=5),sigma=rep(1,5),
+            Theta=rep(1/5,5),xtrue=xtrue,coef=coefs)}
+  stanfit <- stanmodels$logistic
+  print(stanfit)
   # fit <- stan(file=stanfit, data = dat, iter = 1000,chains = 1,thin=2,init=init_fun)
   # # sampling_args <- set_sampling_args(
   # #   object = stanfit, 
