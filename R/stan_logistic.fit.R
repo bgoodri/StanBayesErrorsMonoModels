0fa1cb1be5eacb1a3116cbe0527b637c0faa54c8
gdepalma <gdepalma@apple.com>
2017-11-09 23:28:12 -0800

    Testing 2

--- R/stan_logistic.fit.R
+++ R/stan_logistic.fit.R
@@ -18,18 +18,19 @@ stan_logistic.fit <- function(xobs,yobs,xcens,ycens,xgrid) {
   ycensu[ycens==1] = 1
   
   dat_sav=data.frame(xobs,yobs,xcensl,xcensu,ycensu,ycensl)
-  
+  ?rst
   ### Initialize
   parms=initialize_parms_log(xobs,yobs,xcensu,xcensl)
   coefs=parms$coefs
   xtrue=parms$xtrue
+  
   
   ### Run Stan Model
   dat=list(y=yobs,xgrid=xgrid,Ngrid=Ngrid,n_groups=5,ysig=ysig,N=N,xobs=xobs,xsig=xsig,
            xcensl=xcensl,xcensu=xcensu,ycensl=ycensl,ycensu=ycensu)
   init_fun <- function() {list(mu=seq(min(xtrue)+1,max(xtrue)-1,length=5),sigma=rep(1,5),
             Theta=rep(1/5,5),xtrue=xtrue,coef=coefs)}
-  fit <- sampling(compiled_model, data = dat, iter = 1000,chains = 1,thin=2,init=init_fun)
+  fit <- sampling('../exec/logistic.stan', data = dat, iter = 1000,chains = 1,thin=2,init=init_fun)
   
   thetas <- extract(fit, pars = "beta", inc_warmup = TRUE, permuted = FALSE)
 
