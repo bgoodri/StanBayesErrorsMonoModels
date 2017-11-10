dcaff93eedd42ae04ea7b516852e62c367e384b9
gdepalma <gdepalma@apple.com>
2017-11-09 23:09:17 -0800

    Update 1

--- R/stan_logistic.fit.R
+++ R/stan_logistic.fit.R
@@ -1,9 +1,17 @@
 
-stan_logistic.fit <- function(xobs,yobs,xcens,ycens) {
+
+#' @rdname stan_logistic
+#' @export
+#' @param x A design matrix.
+#' @param y A response variable, which must be a (preferably ordered) factor.
+#' @param wt A numeric vector (possibly \code{NULL}) of observation weights.
+#' @param offset A numeric vector (possibly \code{NULL}) of offsets.
+#' 
+#' @importFrom utils head tail
+stan_logistic.fit <- function(xobs,yobs,xcens,ycens,xgrid) {
   
-  ### Initialize
-  xsig=.707; ysig=2.121
-  Ngrid=1000
+  ### Set up data
+  Ngrid=length(xgrid)
   N=length(xobs)
   xcensl=rep(0,N)
   xcensl[xcens==-1] = 1
@@ -14,34 +22,47 @@ stan_logistic.fit <- function(xobs,yobs,xcens,ycens) {
   ycensu=rep(0,N)
   ycensu[ycens==1] = 1
   
-  
   dat_sav=data.frame(xobs,yobs,xcensl,xcensu,ycensu,ycensl)
   
-  if(sum(xcensu)>0 & sum(xcensl)>0){
-    xgrid=seq(min(xobs)-4,max(xobs)+4,length=1000)
-  }else if(sum(xcensu)>0 & sum(xcensl)==0){
-    xgrid=seq(min(xobs)-1,max(xobs)+4,length=1000)
-  }else if(sum(xcensu)==0 & sum(xcensl)>0){
-    xgrid=seq(min(xobs)-4,max(xobs)+1,length=1000)
-  }else{
-    xgrid=seq(min(xobs)-1,max(xobs)+1,length=1000)
-  }
-  
+  ### Initialize
   parms=initialize_parms_log(xobs,yobs,xcensu,xcensl)
-  coefs=parms$icoefs1
+  coefs=parms$coefs
   xtrue=parms$xtrue
   
-  
-  ### Stan
-  
+  ### Run Stan Model
   dat=list(y=yobs,xgrid=xgrid,Ngrid=Ngrid,n_groups=5,ysig=ysig,N=N,xobs=xobs,xsig=xsig,
            xcensl=xcensl,xcensu=xcensu,ycensl=ycensl,ycensu=ycensu)
   init_fun <- function() {list(mu=seq(min(xtrue)+1,max(xtrue)-1,length=5),sigma=rep(1,5),
-                               Theta=rep(1/5,5),xtrue=xtrue,coef=coefs)}
-  fit <- sampling(stanmodels$logistic, data = dat, iter = 1000,chains = 1,thin=2,
-                  init=init_fun,verbose=TRUE)
+            Theta=rep(1/5,5),xtrue=xtrue,coef=coefs)}
+  fit <- sampling(compiled_model, data = dat, iter = 1000,chains = 1,thin=2,init=init_fun)
   
-  return(fit)
+  thetas <- extract(fit, pars = "beta", inc_warmup = TRUE, permuted = FALSE)
 
 }
 
+                                                                                                       ]]]]]]]]]]
+# internal ----------------------------------------------------------------
+
+# Create "prior.info" attribute needed for prior_summary()
+#
+# @param prior, prior_counts User's prior and prior_counts specifications
+# @return A named list with elements 'prior' and 'prior_counts' containing 
+#   the values needed for prior_summary
+summarize_polr_prior <- function(prior, prior_counts, shape=NULL, rate=NULL) {
+  flat <- !length(prior)
+  prior_list <- list(
+    prior = list(
+      dist = ifelse(flat, NA, "R2"),
+      location = ifelse(flat, NA, prior$location),
+      what = ifelse(flat, NA, prior$what)
+    ), 
+    prior_counts = list(
+      dist = "dirichlet",
+      concentration = prior_counts
+    )
+  )
+  if ((!is.null(shape) && shape > 0) && (!is.null(rate) && rate > 0))
+    prior_list$scobit_exponent <- list(dist = "gamma", shape = shape, rate = rate)
+  
+  return(prior_list)
+}
