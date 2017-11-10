7afecf4b1cec366a20e7f17fa1515158cb265659
gdepalma <gdepalma@apple.com>
2017-11-09 23:12:01 -0800

    Update 2

--- R/stan_logistic.fit.R
+++ R/stan_logistic.fit.R
@@ -2,12 +2,7 @@
 
 #' @rdname stan_logistic
 #' @export
-#' @param x A design matrix.
-#' @param y A response variable, which must be a (preferably ordered) factor.
-#' @param wt A numeric vector (possibly \code{NULL}) of observation weights.
-#' @param offset A numeric vector (possibly \code{NULL}) of offsets.
-#' 
-#' @importFrom utils head tail
+
 stan_logistic.fit <- function(xobs,yobs,xcens,ycens,xgrid) {
   
   ### Set up data
@@ -38,31 +33,4 @@ stan_logistic.fit <- function(xobs,yobs,xcens,ycens,xgrid) {
   
   thetas <- extract(fit, pars = "beta", inc_warmup = TRUE, permuted = FALSE)
 
-}
-
-                                                                                                       ]]]]]]]]]]
-# internal ----------------------------------------------------------------
-
-# Create "prior.info" attribute needed for prior_summary()
-#
-# @param prior, prior_counts User's prior and prior_counts specifications
-# @return A named list with elements 'prior' and 'prior_counts' containing 
-#   the values needed for prior_summary
-summarize_polr_prior <- function(prior, prior_counts, shape=NULL, rate=NULL) {
-  flat <- !length(prior)
-  prior_list <- list(
-    prior = list(
-      dist = ifelse(flat, NA, "R2"),
-      location = ifelse(flat, NA, prior$location),
-      what = ifelse(flat, NA, prior$what)
-    ), 
-    prior_counts = list(
-      dist = "dirichlet",
-      concentration = prior_counts
-    )
-  )
-  if ((!is.null(shape) && shape > 0) && (!is.null(rate) && rate > 0))
-    prior_list$scobit_exponent <- list(dist = "gamma", shape = shape, rate = rate)
-  
-  return(prior_list)
 }
